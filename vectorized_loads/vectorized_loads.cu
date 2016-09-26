8133465ea2ff3bb50ce20b1366600e7f3b11b5d1
Praveen Narayanan <prav.narayanan@gmail.com>
2016-09-25 22:08:14 -0700

    Added vectorized load experiment

---
+++ vectorized_loads/vectorized_loads.cu
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cuda.h>
+//#include <Timer.h>
+#define N 2048
+#define BLOCK_SIZE 128
+
+inline double calc_bw(double time){
+  return bw = N*BLOCK_SIZE*sizeof(int)/(time/1e3)*1e-6;
+}
+  
+
+//I am planning to use this to improve performance in atomics kernels
+
+//from Justin Luitjens' blog post in parallelforall (CUDA Pro Tip: Increase performance with vectorized loads)
+//https://devblogs.nvidia.com/parallelforall/cuda-pro-tip-increase-performance-with-vectorized-memory-access/
+__global__ void device_copy_vec4_kernel(int *d_in, int *d_out){
+  int idx = blockIdx.x*blockDim.x + threadIdx.x;
+  //works best if N is a multiple of 4
+  //assume that N is a multiple of 4 for now
+  for(int i=idx; i<N/4; i += blockDim.x*gridDim.x)
+    reinterpret_cast<int4*> (d_out)[i] = reinterpret_cast<int4*>(d_in)[i];
+}
+
+
+
+void device_copy_vec4(int *d_in, int *d_out){
+   int threads = BLOCK_SIZE;
+   int blocks = N/4;
+   device_copy_vec4_kernel<<<blocks,threads>>>(d_in, d_out);
+}
+
+
+
+__global__ void device_copy_vec1_kernel(int *d_in, int *d_out){
+  int idx = blockIdx.x*blockDim.x + threadIdx.x;
+
+  for(int i=idx; i<N; i+=blockDim.x*gridDim.x)
+    d_out[i] = d_in[i];
+}
+						
+
+void device_copy_vec1(int *d_in, int *d_out){
+  int threads = BLOCK_SIZE;
+  int blocks  = N;
+  device_copy_vec1_kernel<<<blocks, threads>>> (d_in, d_out);
+}
+
+int main(){
+
+  int *h_a;
+  int *d_a_vec4;
+  int *d_a_vec1;
+
+
+  h_a = new int[N*BLOCK_SIZE];
+  cudaMalloc((void **) &d_a_vec4,N*BLOCK_SIZE*sizeof(int));
+  cudaMalloc((void **) &d_a_vec1,N*BLOCK_SIZE*sizeof(int));
+
+
+  for(int i=0; i<N*BLOCK_SIZE; i++){
+    h_a[i]=i;
+  }
+
+
+  cudaMemcpy(d_a_vec1, h_a, N*BLOCK_SIZE, cudaMemcpyHostToDevice);
+
+
+  Timer timer;
+
+  timer.startTimer();
+  device_copy_vec4(d_a_vec1, d_a_vec4);
+  timer.stopTimer();
+
+  timer.getElapsedTime();
+
+  std::cout << "bandwidth = " << calc_bw(timer.getElapsedTime()) << std::endl;
+    
+
+    
+  
+
+  delete [] h_a;
+  cudaFree(d_a_vec4);
+  cudaFree(d_a_vec1);
+  
+  
+}    
+
+
+  
+  
+  
+
+
+
+  
+
+
